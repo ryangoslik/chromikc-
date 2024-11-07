44999c1de91efcfaa381b84bc9a6f23d840d044b
Konrad Bienkowski <konrad.bienkowski1@o2.pl>
2024-11-07 17:00:45 +0100

    123

---
+++ c++/ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿
+
+#include <iostream>
+using namespace std;
+inline int dodaj(int, int);//deklaracja
+int dodaj_zwykla(int, int);//deklaracja
+
+
+int main()
+{
+    int a = 2;
+    int b = 3;
+    int c = 0;
+
+    c = dodaj(a, b);
+    //rownoważne
+    c = a + b;
+
+    cout << c;
+
+    c = dodaj_zwykla(a, b);
+}
+
+inline int dodaj(int a, int b) 
+{
+    return a + b;
+}
+
+int dodaj_zwykla(int a, int b) 
+{
+    return a + b;
+}
+
